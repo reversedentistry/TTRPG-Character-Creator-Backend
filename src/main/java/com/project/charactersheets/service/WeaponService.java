6a5b929608cd77dd8ba5c229b67fee2c26ca51e4
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2023-04-12 16:27:43 -0700

    Added service method to add a new weapon and get all weapons per owner

--- src/main/java/com/project/charactersheets/service/WeaponService.java
+++ src/main/java/com/project/charactersheets/service/WeaponService.java
@@ -1,9 +1,22 @@
 package com.project.charactersheets.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.project.charactersheets.model.Weapon;
 import com.project.charactersheets.repository.WeaponRepository;
 
+import java.util.List;
+
 @Service
 public class WeaponService {
+    @Autowired
+    WeaponRepository weaponRepo;
+
+    public Weapon addNewWeapon(Weapon newWeapon) {
+        return weaponRepo.save(newWeapon);
+    }
+
+    public List<Weapon> getCharacterWeapons(int charId) {
+        return weaponRepo.findAllByOwnerId(charId);
+    }
 }
