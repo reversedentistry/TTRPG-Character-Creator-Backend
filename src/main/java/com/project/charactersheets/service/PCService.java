2d5af9c1bd781c6ead9082d54539b9acf521870a
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2023-04-12 16:23:52 -0700

    Added method to retrieve a particular character

--- src/main/java/com/project/charactersheets/service/PCService.java
+++ src/main/java/com/project/charactersheets/service/PCService.java
@@ -5,6 +5,8 @@ import com.project.charactersheets.repository.PCRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class PCService {
     @Autowired
@@ -12,5 +14,10 @@ public class PCService {
 
     public PlayerCharacter createNewCharacter(PlayerCharacter pc) {
         return pcRepository.save(pc);
+    }
+
+    public Optional<PlayerCharacter> retrieveCharacter(int pcId) {
+        Optional<PlayerCharacter> pc = pcRepository.findById(pcId);
+        return pc;
     }
 }
