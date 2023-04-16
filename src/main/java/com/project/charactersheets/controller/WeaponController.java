24be8e9497afdeae3259655c56f68fe5a2fd5b5e
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2023-04-15 22:23:07 -0700

    Created weapon controller class

---
+++ src/main/java/com/project/charactersheets/controller/WeaponController.java
@@ -0,0 +1,27 @@
+package com.project.charactersheets.controller;
+
+import com.project.charactersheets.model.Weapon;
+import com.project.charactersheets.service.WeaponService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/weapons")
+public class WeaponController {
+    @Autowired
+    WeaponService weaponService;
+
+    @PostMapping
+    public ResponseEntity<Weapon> addWeapon(Weapon weapon) {
+        weaponService.addNewWeapon(weapon);
+        return ResponseEntity.status(200).body(weapon);
+    }
+
+    @GetMapping("/{pcId}")
+    public List<Weapon> getAllPcWeapons(@PathVariable int pcId) {
+        return weaponService.getCharacterWeapons(pcId);
+    }
+}
