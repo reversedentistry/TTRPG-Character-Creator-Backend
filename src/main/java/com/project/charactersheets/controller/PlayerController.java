3b0067ac9894d8819b54f10eb76863a183268b0d
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2023-04-16 18:07:32 -0700

    Created controller, service, and repository layers for creating new players

---
+++ src/main/java/com/project/charactersheets/controller/PlayerController.java
@@ -0,0 +1,22 @@
+package com.project.charactersheets.controller;
+
+import com.project.charactersheets.model.Player;
+import com.project.charactersheets.service.PlayerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/players")
+public class PlayerController {
+    @Autowired
+    PlayerService playerService;
+
+    @PostMapping
+    public ResponseEntity<Player> createPlayer(Player player) {
+        Player newPlayer = playerService.createNewPlayer(player);
+        return ResponseEntity.status(200).body(newPlayer);
+    }
+}
---
+++ src/main/java/com/project/charactersheets/service/PlayerService.java
@@ -0,0 +1,16 @@
+package com.project.charactersheets.service;
+
+import com.project.charactersheets.model.Player;
+import com.project.charactersheets.repository.PlayerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PlayerService {
+    @Autowired
+    PlayerRepository playerRepo;
+
+    public Player createNewPlayer(Player player) {
+        return playerRepo.save(player);
+    }
+}
