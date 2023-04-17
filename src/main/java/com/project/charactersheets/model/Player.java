73cf90c00ca092c2e88198baefa209c3fda14efc
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2023-04-16 17:49:53 -0700

    Added Lombok, generation type, and relationship annotations

--- src/main/java/com/project/charactersheets/model/Player.java
+++ src/main/java/com/project/charactersheets/model/Player.java
@@ -1,15 +1,22 @@
 package com.project.charactersheets.model;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.List;
 
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 @Entity
 public class Player {
     @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private int playerId;
     private String playerName;
 
     @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "player", referencedColumnName = "playerId")
     private List<PlayerCharacter> characters;
 }
