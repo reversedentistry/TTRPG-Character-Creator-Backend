e5c3d0d00697c8537d9990186c527a8505e353bd
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2023-04-24 20:13:02 -0700

    Added extra identification fields

--- src/main/java/com/project/charactersheets/model/Player.java
+++ src/main/java/com/project/charactersheets/model/Player.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.*;
 
 import java.util.List;
+import java.util.Objects;
 
 @Getter
 @Setter
@@ -20,8 +21,30 @@ public class Player {
     @org.springframework.lang.NonNull
     private String playerName;
 
+    @NonNull
+    @Column(unique = true)
+    @org.springframework.lang.NonNull
+    private String username;
+
+    @NonNull
+    @org.springframework.lang.NonNull
+    private String password;
+
     @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     @JoinColumn(name = "player", referencedColumnName = "playerId")
     @ToString.Exclude
     private List<PlayerCharacter> characters;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Player player = (Player) o;
+        return playerId.equals(player.playerId) && playerName.equals(player.playerName) && Objects.equals(characters, player.characters);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(playerId, playerName, characters);
+    }
 }
